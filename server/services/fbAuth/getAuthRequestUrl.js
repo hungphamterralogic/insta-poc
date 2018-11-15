0e9a679c1807c56a04096ac0cc6504bbdfa550a6
hungphamterralogic <hung.pham@terralogic.com>
2018-11-15 19:29:59 +0700

    chore: move permission scopes to top config file

--- server/services/fbAuth/getAuthRequestUrl.js
+++ server/services/fbAuth/getAuthRequestUrl.js
@@ -1,10 +1,13 @@
 import fetchFromApiMiddleman from "../utils/fetchFromApiMiddleman";
+import { REQUIRED_PERMISSIONS_SCOPES } from "../../../config";
 
 module.exports = async () => {
-  const scopes = ["email", "user_photos"];
+  const scopes = REQUIRED_PERMISSIONS_SCOPES;
   const scope = scopes.join(" ");
 
-  const response = await fetchFromApiMiddleman("/auth/getAuthRequestUrl", { scope });
+  const response = await fetchFromApiMiddleman("/auth/getAuthRequestUrl", {
+    scope
+  });
 
   return response;
 };
