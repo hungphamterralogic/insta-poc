83dc11d3cfcfcdb0c8f10f25862aa89191a2950d
hungphamterralogic <hung.pham@terralogic.com>
2018-11-15 18:57:42 +0700

    feat: implement fetch fb api in middleman server

--- middleman-server/handlers/handleFbApi.js
+++ middleman-server/handlers/handleFbApi.js
@@ -1,3 +1,30 @@
+import axios from "axios";
+import to from "await-to-js";
+import queryString from "query-string";
+import lodashIsEmpty from "lodash.isempty";
+import { API_URL } from "../config";
+
 module.exports = async (req, res) => {
-  res.json({ stat: "OK", message: "Got here" });
+  const { relativeApiUrl, accessToken } = req.query;
+
+  const queryInRelativeApiUrl = queryString.parseUrl(relativeApiUrl).query;
+
+  const relativeApiUrlHasQuery = !lodashIsEmpty(queryInRelativeApiUrl);
+
+  const requestUrl = relativeApiUrlHasQuery
+    ? `${API_URL}/${relativeApiUrl}&access_token=${accessToken}`
+    : `${API_URL}/${relativeApiUrl}?access_token=${accessToken}`;
+
+  const [err, response] = await to(axios.get(requestUrl));
+
+  if (response && response.data && !response.data.error) {
+    res.json({ stat: "OK", data: response.data });
+  } else {
+    const message = err
+      ? err.message
+      : response && response.data && response.data.error
+      ? response.data.error.message
+      : "Something bad happened.";
+    res.json({ message });
+  }
 };
